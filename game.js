66bca099e7b923bb0eb628bd8886b8e12122f6aa
ShrineSigdel <123295702+ShrineSigdel@users.noreply.github.com>
2023-12-08 19:03:52 +0545

    Fixed winnning combo error

--- game.js
+++ game.js
@@ -60,8 +60,8 @@ const Gameboard = (() => {
 const checkWinningCondtion = (board) => {
   const winningConditions = [
     [0, 1, 2],
-    [1, 2, 3],
     [3, 4, 5],
+    [6, 7, 8],
     [0, 4, 8],
     [2, 4, 6],
     [0, 3, 6],
